7731babd3c1d57ac8362fa6daafd04c8655b849f
oliver.jung <oliver.jung@nsgroup.ch>
2024-06-21 10:31:46 +0200

    updates

--- scripts.js
+++ scripts.js
@@ -7,3 +7,28 @@ function returnText() {
 }
 
 console.log(localStorage.getItem('input'));
+
+
+
+// DATE
+
+let result = getWeekNumber(new Date());
+console.log("result[1]", result[1]);
+
+function getWeekNumber(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    return [d.getUTCFullYear(), weekNo];
+}
+
+let today = new Date();
+let dd = String(today.getDate());
+let mm = String(today.getMonth());
+let yyyy = today.getFullYear();
+months = new Array('Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember');
+curMonth = months[mm];
+today = dd + '. ' + curMonth + ' ' + yyyy;
+
+document.querySelector('.js-date').textContent = today;
